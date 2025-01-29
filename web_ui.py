bd8db0328409db4882851c6ab2e9ca3e68e222b0
henzelis <henzelis@gmail.com>
2025-01-29 14:59:05 +0200

    dark theme update

--- web_ui.py
+++ web_ui.py
@@ -22,7 +22,7 @@ site_url = st.text_input("Enter Site URL", value="https://www.i.ua")
 #     default=["Response time (ms)"]
 # )
 parameter_options = st.segmented_control(
-    "Graphs", options=["Response time (ms)", "Availability (%)"], default=["Response time (ms)"], selection_mode="multi"
+    "Graphs", options=["Response time (ms)", "Availability (%)"], default=["Response time (ms)", "Availability (%)"], selection_mode="multi"
 )
 
 refresh_time = st.selectbox("Choose refresh time (sec):", [10, 15, 30])
@@ -39,14 +39,21 @@ refresh_time = st.selectbox("Choose refresh time (sec):", [10, 15, 30])
 #         st.line_chart(df.set_index("time")["response_time"])
 #     time.sleep(refresh_time)
 
+if st.button("Clear Database"):
+    db = TinyDB('local_db.json', indent=4, separators=(',', ': '))
+    query_db = Query()
+    db.remove(query_db.site_url == site_url)
+
+col1, col2 = st.columns(2)
 
 placeholder = st.empty()
 while True:
     with placeholder.container():
-        status, response_time, current_time = site_avail_engine.check_website_performance(site_url)
         st.subheader(f"Site {site_url} Response Time")
+        col1, col2 = st.columns(2)
+        status, response_time, current_time = site_avail_engine.check_website_performance(site_url)
         current_data = {'site_url': site_url, 'availability': status, 'response_time': response_time, 'time': current_time}
-        db = TinyDB('local_db.json')
+        db = TinyDB('local_db.json', indent=4, separators=(',', ': '))
         db.insert(current_data)
         query_db = Query()
         df = pd.DataFrame(db.search(query_db.site_url == site_url))
@@ -54,8 +61,13 @@ while True:
         print(df)
         # st.snow()
         if "Response time (ms)" in parameter_options:
-            st.line_chart(df.set_index("time")["response_time"], x_label='Time', y_label='Response Time')
+            with col1:
+                st.subheader("Response Time")
+                st.line_chart(df.set_index("time")["response_time"], x_label='Time', y_label='Response Time', width=800, height=400, use_container_width=False)
         if "Availability (%)" in parameter_options:
-            st.line_chart(df.set_index("time")["availability"], x_label='Time', y_label='Availability')
+            with col2:
+                st.subheader("Availability")
+                st.line_chart(df.set_index("time")["availability"], x_label='Time', y_label='Availability', width=800, height=400, use_container_width=False)
     time.sleep(refresh_time)
+
 
